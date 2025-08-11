aba5a776e30796d00f90576bd143afdcd419f4b6
bbppmpvboemalang-ux <bbppmpvboemalang@gmail.com>
2025-08-11 13:23:50 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showIcon(IconNames.Ghost)
+basic.pause(2000)
+basic.showString("cinta")
+for (let index = 0; index < 4; index++) {
+    basic.clearScreen()
+}
+basic.forever(function () {
+	
+})
